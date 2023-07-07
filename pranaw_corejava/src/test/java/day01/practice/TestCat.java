6a674166e1d67d776ae6f3f041aa9d5bf933d72c
cyber-sparky <mdpranaw@gmail.com>
2023-07-07 09:22:31 +0530

    day02 solved

---
+++ pranaw_corejava/src/test/java/day01/practice/TestCat.java
@@ -0,0 +1,11 @@
+package day01.practice;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+public class TestCat {
+	
+	@Test
+	public void testGetAge() {
+		Assertions.assertTrue(true);
+	}
+}
