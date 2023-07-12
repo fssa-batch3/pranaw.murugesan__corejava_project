e13054d9c58ea6b47dcc1f6499393bfe79bb2395
cyber-sparky <mdpranaw@gmail.com>
2023-07-12 23:35:01 +0530

    day 05 commit

--- pranaw_corejava/src/main/java/day05/practice/Main.java
+++ pranaw_corejava/src/main/java/day05/practice/Main.java
@@ -4,6 +4,11 @@ class Department {
 	String deptName;
 	int deptId;
 	
+	public Department(String deptName,int deptId) {
+		this.deptName=deptName;
+		this.deptId=deptId;
+	}
+	
 }
 
 class Student {
@@ -12,21 +17,35 @@ class Student {
 	Department department;
 	
 	// Create a constructor and assign depart and respective attributes
+	// Add toString method to get the Student details. 
 	
-	Student(String name,int id){
-		this.name = name;
-		this.id = id;
+	public  Student(String name,int id,Department department) {
 		
+		this.name=name;
+		this.id=id;
+		this.department=department;
+
+	}
+	public void getStudent() {
+		
+		System.out.println("Student details");
+		System.out.println("department name : " + department.deptName);
+		System.out.println("Department id   : " + department.deptId);
+		System.out.println("Student Name    : " + name);
+		System.out.println("Student id      : " + id);
 	}
 	
-	// Add toString method to get the Student details. 
-	@Override
-    public String toString() {
-        return "Student [name =" + name + ", id =" + id + "]";
-    }
-}
-public class Main{
 	
 }
+
 // Create a main class to create a Student and Department objects 
 // and print the details
+
+public class Main {
+	public static void main(String[] args) {
+		Department depart=new Department("Developer", 4);
+		Student std=new Student("Pranaw", 4, depart);
+		std.getStudent();
+	}
+
+}
