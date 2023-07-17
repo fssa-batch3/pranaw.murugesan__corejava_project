9370c9005f3676bcf181b46c798842528c480029
cyber-sparky <mdpranaw@gmail.com>
2023-07-17 19:43:30 +0530

    day 08 practice

--- pranaw_corejava/src/main/java/day07/practice/Main.java
+++ pranaw_corejava/src/main/java/day07/practice/Main.java
@@ -1,49 +1,41 @@
 package day07.practice;
-
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.*;
 
-public class RemoveDuplicateTask {
+
+public class Main {
 	public static void main(String[] args) {
 		ArrayList<Task> taskList = new ArrayList<>();
-
 		// Adding tasks to the ArrayList
 		taskList.add(new Task(1, "Task 1", LocalDate.of(2023, 7, 15)));
 		taskList.add(new Task(2, "Task 2", LocalDate.of(2023, 7, 15)));
 		taskList.add(new Task(3, "Task 1", LocalDate.of(2023, 7, 15)));
 		taskList.add(new Task(4, "Task 3", LocalDate.of(2023, 7, 16)));
 		taskList.add(new Task(5, "Task 1", LocalDate.of(2023, 7, 15)));
-		
 		printTasks(taskList); // arraylist
-		removeDuplicates(taskList); //hashset
-
+		removeDuplicates(taskList); // hashset
 	}
-
 //		a methd to printing tasks
 	public static void printTasks(ArrayList<Task> tasks) {
-		for (Task task : tasks) {
-			System.out
-					.println("ID: " + task.getId() + ", Name: " + task.getName() + ", Deadline: " + task.getDeadline());
-		}
-		System.out.println();
 	}
 // 		a method to remove duplicates
 	public static void removeDuplicates(ArrayList<Task> taskList) {
 		HashSet<Task> taskSet = new HashSet<>(taskList);
 		taskList.clear();
 		taskList.addAll(taskSet);
+		for (Task task : taskSet) {
+			System.out
+					.println("ID: " + task.getId() + ", Name: " + task.getName() + ", Deadline: " + task.getDeadline());
+		}
+		System.out.println();
 	}
-
 }
-
 //	task class
 class Task {
 	private int id;
 	private String name;
 	private LocalDate deadline;
-
-	public Task(int id, String name, LocalDate deadline) { // constructor to save details
+	public Task(int id, String name, LocalDate deadline) {
 		this.id = id;
 		this.name = name;
 		this.deadline = deadline;
@@ -52,17 +44,17 @@ class Task {
 	public int getId() {
 		return id;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public LocalDate getDeadline() {
 		return deadline;
 	}
-
+	public int hashCode() {
+		return Objects.hash(this.deadline, this.name);
+	}
 	@Override
-	public boolean equals(Object obj) { // adding validation if equals or not
+	public boolean equals(Object obj) {
 		if (this == obj) {
 			return true;
 		}
@@ -70,6 +62,6 @@ class Task {
 			return false;
 		}
 		Task other = (Task) obj;
-		return id == other.id && name.equals(other.name) && deadline.equals(other.deadline);
+		return name.equals(other.name) && deadline.equals(other.deadline);
 	}
 }
