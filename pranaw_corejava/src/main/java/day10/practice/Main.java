83dabc92b57d770d510b8b767688e56867a398c4
cyber-sparky <mdpranaw@gmail.com>
2023-07-20 00:07:59 +0530

    test case updated

--- pranaw_corejava/src/main/java/day10/practice/Main.java
+++ pranaw_corejava/src/main/java/day10/practice/Main.java
@@ -21,7 +21,7 @@ class EmailValidator {
 		if (matches) {
 			return true;
 		} else {
-			throw new InvalidEmailException("Invalid email format: " + emailId);
+			throw new InvalidEmailException("Invalid email format");
 		}
 	}
 }
@@ -35,7 +35,7 @@ public class Main {
 				System.out.println("Valid email: " + email);
 			}
 		} catch (InvalidEmailException e) {
-			System.out.println("Error: " + e.getMessage());
+			System.out.println(e.getMessage());
 		}
 	}
 }
--- pranaw_corejava/src/main/java/day10/practice/UserExist.java
+++ pranaw_corejava/src/main/java/day10/practice/UserExist.java
@@ -2,32 +2,60 @@ package day10.practice;
 
 import java.util.ArrayList;
 import java.util.Objects;
+import java.util.regex.Pattern;
 
 class User {
 	private int id;
 	private String name;
 	private String emailId;
-	public User(int id, String name, String email){
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public User(int id, String name, String email) {
 		this.id = id;
 		this.name = name;
 		this.emailId = email;
 	}
-    // Override the equals method to compare the fields of User objects
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return id == user.id && name.equals(user.name) && emailId.equals(user.emailId);
-    }
 
-    // Don't forget to override hashCode whenever you override equals
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, emailId);
-    }
+	// Override the equals method to compare the fields of User objects
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		User user = (User) o;
+		return id == user.id && name.equals(user.name) && emailId.equals(user.emailId);
+	}
+
+	// Don't forget to override hashCode whenever you override equals
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name, emailId);
+	}
 }
-
 
 class UserAlreadyExistsException extends Exception {
 
@@ -45,13 +73,33 @@ public class UserExist {
 		if (UserExist.user.contains(user)) {
 			throw new UserAlreadyExistsException("User already exist");
 		}
+		validateId(user.getId());
+		validateEmail(user.getEmailId());
 		UserExist.user.add(user);
+
 		return true;
 	}
 
+	public static boolean validateId(int id) throws IllegalArgumentException {
+		if (id < 0) {
+			throw new IllegalArgumentException("id number cannot be negative");
+		}
+		return true;
+	}
+
+	public static boolean validateEmail(String email) throws IllegalArgumentException {
+		String regex = "^[A-Za-z0-9+_.-]+@(.+)$";
+		boolean matches = Pattern.compile(regex).matcher(email).matches();
+		if (matches) {
+			return true;
+		} else {
+			throw new IllegalArgumentException("mail is invalid");
+		}
+	}
+
 	public static void main(String[] args) throws UserAlreadyExistsException {
-		User user1 = new User(1,"pranaw","pmurugesan@fssa.fresworks.com");
-		User user2 = new User(1,"pranaw","pmurugesan@fssa.fresworks.com");
+		User user1 = new User(1, "pranaw", "pmurugesan@fssa.fresworks.com");
+		User user2 = new User(1, "pranaw", "pmurugesan@fssa.fresworks.com");
 		try {
 			register(user1);
 			register(user2);
